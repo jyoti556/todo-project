5fe2e1a8d74d604308d87611204efd609c8c0311
Jyoti Dhanak <jd1@gmail.com>
2024-03-17 00:53:55 +0530

    removed auth0

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider
-    domain="dev-ndsqm1ra07x0fnop.us.auth0.com"
-    clientId="54RPdoIsAphWFT5smx9xzrlh3ngtraCq"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
+  
     <App />
-  </Auth0Provider>
+  
     
   
 );
